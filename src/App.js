4c134eefe8612d17cfbd6eac040ee30411af663c
m4theuspereira.maker <matheusmonaco1@hotmail.com>
2020-07-31 15:15:27 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from'./Card';
-import faker from 'faker'
+import faker from 'faker'; 
+import styled, {ThemeProvider,css} from 'styled-components'; 
+import Button from './element/Button'; 
+
+const theme ={
+  primary: '4caf50',
+  mango: 'yellow'
+}
+
 
 
 function App() {
@@ -10,16 +18,19 @@ function App() {
     const [name, setName] = useState('Alan Smith');
     const [cards, setCards] = useState([
       {
+        id:'1',
         name: 'Jackie Brown',
         title: 'Airline agent', 
         avatar: faker.image.avatar()
       },
       {
+        id: '2',
         name: 'Ton Clancy',
         title: 'Writer', 
         avatar: faker.image.avatar()
       },
       {
+        id:'3',
         name:'Roberto Machado',
         title: 'Investigador Particular', 
         avatar: faker.image.avatar()
@@ -38,21 +49,41 @@ function App() {
       setCards(cards_copy)
 
     }    
-       
+
+    const changeNameHandler = (event, id) => {
+      //1. which card
+      const cardIndex = cards.findIndex(card => card.id == id)
+      //2. make a copy of the cards
+      const cards_copy = [...cards]
+      //3. change the name of the specific card
+      cards_copy[cardIndex].name = event.target.value
+      //4. set the cards with the latest version of card copy
+      setCards(cards_copy)
+    }
+    
+    const classes = ['button'];
+    if (cards.length < 3) classes.push('pink')
+    if (cards.length < 2) classes.push('red text');
        const cardsMarkup = showCard &&( 
           cards.map((card, index)=> <Card
           avatar={card.avatar}
           name={card.name}
           title={card.title}
+          key={card.id}
           onDelete={()=>deleteCardHandler(index)}
+          onChangeName ={(event) => changeNameHandler(event, card.id)}
         />)
        )
 
     return (
+      <ThemeProvider theme={theme}>
       <div className="App">
-          <button className="button button-red" onClick={toggleShowCard}>toggleShowCard</button>
+          <Button  color="mango" length={cards.length}>Toggle</Button>
+          <button className={classes.join(' ')} onClick={toggleShowCard}>toggleShowCard</button>
+        
           {cardsMarkup}
       </div>
+      </ThemeProvider>
     );
   }
 
--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,8 @@ const Card = (props) =>{
         <img src={props.avatar} alt="avatar" style={{width: '100%'}}/>
       <div className="container">
              <h4><b>{props.name}</b></h4>
-            <p>{props.title}</p>           
+            <p>{props.title}</p>         
+            <input type="text" onChange={props.onChangeName} value={props.name}/> 
             <p><button className="button button-blue" onClick={props.onDelete} >Delete</button></p>
             <div>{props.children}</div>
       </div>
